6176ff7ce9922e6321aa21a55027346b38671ac9
Patrick Muehlbauer <patrick@opentrash.org>
2017-01-22 21:44:43 +0100

    Update version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exscm.Mixfile do
 
   def project do
     [app: :exscm,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3",
      description: description(),
      build_embedded: Mix.env == :prod,
